37761d82fd11a6742725d1437275cfd9c739fea7
Lopingsuee <umaygans03@gmail.com>
2026-05-11 12:44:29 +0700

    Kriteria 2 → Skilled (3 pts)

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from utils.extract import scrape_all_pages
-from utils.load import save_to_csv
+from utils.load import (
+    save_to_csv,
+    save_to_postgresql,
+)
 from utils.transform import transform_data
 
 
@@ -9,6 +12,8 @@ def main():
     clean_data = transform_data(products)
 
     save_to_csv(clean_data)
+
+    save_to_postgresql(clean_data)
 
 
 if __name__ == "__main__":
--- utils/load.py
+++ utils/load.py
@@ -1,3 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+
+load_dotenv()
+
+
 def save_to_csv(dataframe, filename="products.csv"):
     try:
         dataframe.to_csv(filename, index=False)
@@ -6,3 +15,36 @@ def save_to_csv(dataframe, filename="products.csv"):
 
     except Exception as error:
         print(f"Failed to save CSV: {error}")
+
+
+def save_to_postgresql(
+    dataframe,
+    table_name="fashion_products",
+):
+    try:
+        user = os.getenv("DB_USER")
+        password = os.getenv("DB_PASSWORD")
+        host = os.getenv("DB_HOST")
+        port = os.getenv("DB_PORT")
+        database = os.getenv("DB_NAME")
+
+        url = (
+            f"postgresql://{user}:{password}"
+            f"@{host}:{port}/{database}"
+        )
+
+        engine = create_engine(url)
+
+        dataframe.to_sql(
+            table_name,
+            engine,
+            if_exists="replace",
+            index=False,
+        )
+
+        print(
+            f"Data saved to PostgreSQL table: {table_name}"
+        )
+
+    except Exception as error:
+        print(f"Failed to save PostgreSQL: {error}")
